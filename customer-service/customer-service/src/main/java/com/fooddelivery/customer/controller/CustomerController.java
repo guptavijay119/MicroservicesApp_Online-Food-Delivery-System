403cd5d40ab31b707275dbb16f013a681b2bb219
guptavijay119 <vijaynic5118@gmail.com>
2026-06-25 17:44:25 +0530

    DONE

--- customer-service/customer-service/src/main/java/com/fooddelivery/customer/controller/CustomerController.java
+++ customer-service/customer-service/src/main/java/com/fooddelivery/customer/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package com.fooddelivery.customer.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +30,17 @@ public class CustomerController {
 
         return repository.save(customer);
     }
+    
+    
+    //find all table data 
+    @GetMapping("/allDataFetch")
+    public List<Customer> getdata() {
+    	
+    	List<Customer> alldata=repository.findAll();
+    	
+    	return alldata;
+    }
+    
     
     
     
